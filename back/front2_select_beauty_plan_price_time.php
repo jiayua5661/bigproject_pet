f544e0466d17e9edb5b69ea375a9b948b5943a35
jiayua5661 <a0975069169@gmail.com>
2024-08-06 11:59:26 +0800

    0806

--- back/front2_select_beauty_plan_price_time.php
+++ back/front2_select_beauty_plan_price_time.php
@@ -9,11 +9,17 @@ $dbname = 'petdb';
 $user = 'root';
 $password = '';
 
-$getdata = file_get_contents('php://input');
-$data = json_decode($getdata, true);
-$pet_species = $data['pet_species'];
-$pet_weight = $data['pet_weight'];
-$pet_fur = $data['pet_fur'];
+// $getdata = file_get_contents('php://input');
+// $data = json_decode($getdata, true);
+// $pet_species = $data['pet_species'];
+// $pet_weight = $data['pet_weight'];
+// $pet_fur = $data['pet_fur'];
+// $planid = $data['planid'];
+
+$pet_species = $_REQUEST['pet_species'];
+$pet_weight = $_REQUEST['pet_weight'];
+$pet_fur = $_REQUEST['pet_fur'];
+$planid = $_REQUEST['planid'];
 $weight_range;
 
 if($pet_species == '狗') {
@@ -32,20 +38,19 @@ if($pet_species == '狗') {
     $weight_range = '0000';
 }
 
-
-
 try {
 
     $db = new PDO("mysql:host=${host};dbname=${dbname};charset=utf8mb4", $user);
-    $sql = "select * from beauty_plan_price_time where pet_species = ? and weight_range = ? and pet_fur = ?";
+    $sql = "select * from beauty_plan_price_time where pet_species = ? and weight_range = ? and pet_fur = ? and planid = ?";
     $stmt =$db->prepare($sql);
-    $stmt->execute([$pet_species, $weight_range, $pet_fur]);
+    $stmt->execute([$pet_species, $weight_range, $pet_fur, $planid]);
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     # fetchAll(PDO::FETCH_ASSOC) fetchAll 是拿全部的陣列  PDO::FETCH_ASSOC 是只顯示KEY是欄位名稱跟value
     #預設顯示兩個 1.KEY是欄位名稱跟value   2.KEY是索引值跟value 
 
     header('Content-Type: application/json');
     echo json_encode($rows);
+    // echo json_encode(['error' => '55555']);
 
     // $response = [
     //     'price' => 50.0,
@@ -56,5 +61,6 @@ try {
     // echo json_encode($response);
     
 } catch (PDOException $e){
-    print($e);
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
 }
